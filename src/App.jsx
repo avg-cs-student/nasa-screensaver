871718fb57023a5aeeb939dca72ed7777a1e2d1a
justincromer <justincromer@protonmail.com>
2022-09-12 07:51:16 -0400

    Added css for img display, randomized date.
    Date is now random month/day from 2021. Need to have page refresh on
    time intervals next.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import Api_Info from "./api.json";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import "./common.css";
 
 const nasaURL = "https://api.nasa.gov/planetary/apod?api_key=";
+
+const getRandInt = max => {
+  return Math.floor(Math.random() * max);
+}
 
 const App = () => {
   const [ APOD_url, setAPOD_url ] = useState(null);
   const [ randomDate, setRandomDate ] = useState(null);
-//randomDate.toLocaleDateString('en-CA')
-  
-  
+
   useEffect(() => {
-    axios.get(nasaURL + Api_Info.apikey + "&date=" + "2001-09-19")
+    let day = getRandInt(30);
+    let month = getRandInt(12);
+    console.log(month);
+    console.log(day);
+
+    axios.get(nasaURL + Api_Info.apikey + "&date=" + "2021-" + month + "-" + day)
       .then(response => {
         console.log(response);
         setAPOD_url(response.data.hdurl);
@@ -19,12 +27,14 @@ const App = () => {
       .catch( error => {
         console.error(error);
       });
+
+
   
   }, []);
 
   return (
-    <div>
-      <img src={APOD_url} alt="Nasa Astronomy Picture of the Day"/>
+    <div class="img-container">
+      <img class="img-center" src={APOD_url} alt="Nasa Astronomy Picture of the Day"/>
     </div>
   );
 };
